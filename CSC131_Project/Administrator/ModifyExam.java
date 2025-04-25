03abf8a685075e5be6452cc33f2ddbd080ca0210
albert-l30 <albert.ljr32@gmail.com>
2025-04-25 15:28:56 -0700

    Update ModifyExam.java

--- CSC131_Project/Administrator/ModifyExam.java
+++ CSC131_Project/Administrator/ModifyExam.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package UserJFrames.Administrator;
+package Administrator;
 
 import java.io.*;
 import java.nio.file.*;
